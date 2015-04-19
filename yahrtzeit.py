9d8ba32a5c25fa35a91fed8425e114db80ebb713
Daniel Bernstein <dbernste@cisco.com>
2015-04-19 15:44:29 +0300

    Add yahrtzeit.py file

---
+++ yahrtzeit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# Read in file, build DB of dates
+
+
+# Feed dates to hebcalendar as "yahrtzeit" type
+
+# Get full date list from hebcalendar, filter for yahrtzeit, shabbat, chag
